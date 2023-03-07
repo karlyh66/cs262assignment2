dfd8784711a991b40d417191149d1de0367ee136
Catherine <catherinehuang@college.harvard.edu>
2023-03-07 13:17:40 -0500

    collected more data, updated notebook

--- client.py
+++ client.py
@@ -155,5 +155,5 @@ if __name__ == "__main__":
         print("Usage: client ID")
         sys.exit(1)
     port = 2000
-    run_no = 9
+    run_no = 4
     Client(port, int(sys.argv[1]), run_no).run()
